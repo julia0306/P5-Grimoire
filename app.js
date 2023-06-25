3b9e6ea6a8f80ff0069ca98bbd7cd8b604b02e21
Julia Taylor <julia.taylor3@gmail.com>
2023-06-25 23:33:13 +0200

    bookSchema (model) implemented and post and get routes made functional

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const Book = require('./models/Book')
+
+mongoose.connect('mongodb+srv://JTaylor:P5Grimoire_JT@p5-grimoire.vwnuvln.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // middleware qui permet d'accéder au corps de la requête (intercepte les requêtes qui contiennent du json et mettent à disposition le corps de la requête req.body):
 app.use(express.json());
@@ -13,250 +22,32 @@ app.use((req, res, next) => {
   });
 
 app.post('/api/books', (req, res, next) => {
-    console.log(req.body);
-    // code 201 pour la création de ressource
-    res.status(201).json({
-        message: "objet créé"
+    //On enlève l'id des données car il est généré automatiquement et pa prévu par le modèle
+    delete req.body._id;
+    const book = new Book({
+        // opérateur spread qui va aller copier les champs qu'il y a dans le body de la request
+        ...req.body
     });
+    book.save()
+        .then(()=> res.status(201).json({message: 'Livre enregistré'}))
+        .catch(error => res.status(400).json({error}))
 })
 
-app.use('/api/books', (req, res, next) => {
-    const book = [{
-        "id": "1",
-        "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-        "title" : "Milwaukee Mission",
-        "author": "Elder Cooper",
-        "imageUrl" : "https://via.placeholder.com/206x260",
-        "year" : 2021,
-        "genre" : "Policier",
-        "ratings" : [{
-          "userId" : "1",
-          "grade": 5
-        },
-          {
-            "userId" : "1",
-            "grade": 5
-          },
-          {
-            "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-            "grade": 5
-          },
-          {
-            "userId" : "1",
-            "grade": 5
-          }],
-        "averageRating": 3
-      },
-        {
-          "id": "2",
-          "userId" : "clbxs3tag6jkr0biul4trzbrv",
-          "title" : "Book for Esther",
-          "author": "Alabaster",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Paysage",
-          "ratings" : [{
-            "userId" : "clbxs3tag6jkr0biul4trzbrv",
-            "grade": 4
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 4.2
-        },
-        {
-          "id": "3",
-          "userId" : "1",
-          "title" : "The Kinfolk Table",
-          "author": "Nathan Williams",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Cuisine",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 3
-        },
-        {
-          "id": "4",
-          "userId" : "1",
-          "title" : "Milwaukee Mission",
-          "author": "Elder Cooper",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2021,
-          "genre" : "Policier",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 3
-        },
-        {
-          "id": "5",
-          "userId" : "1",
-          "title" : "Book for Esther",
-          "author": "Alabaster",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Paysage",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 4
-        },
-        {
-          "id": "6",
-          "userId" : "1",
-          "title" : "The Kinfolk Table",
-          "author": "Nathan Williams",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Cuisine",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 3
-        },
-        {
-          "id": "7",
-          "userId" : "1",
-          "title" : "Milwaukee Mission",
-          "author": "Elder Cooper",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2021,
-          "genre" : "Policier",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 3
-        },
-        {
-          "id": "8",
-          "userId" : "clc7s9xnh7zpt0ak4fisdwuj1",
-          "title" : "Book for Esther",
-          "author": "Alabaster",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Paysage",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 4
-        },
-        {
-          "id": "9",
-          "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-          "title" : "The Kinfolk Table",
-          "author": "Nathan Williams",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Cuisine",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-              "grade": 1
-            }],
-          "averageRating": 3
-        }
-      ];
-    res.status(200).json(book);
+// ajout d'une route dynamique, en fonction de l'id
+app.get('/api/books/:id', (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+      .then(book => res.status(200).json(book))
+      .catch(error => res.status(404).json({ error }));
   });
+
+app.get('/api/books', (req, res, next) => {
+    //méthode find qui retourne tous les objets
+    Book.find()
+    //on retourne le tableau de tous les things trouvés dans la BDD
+        .then(books=> res.status(200).json(books))
+        .catch(error => res.status(400).json({error}))
+});
+
+
 
 module.exports = app;
